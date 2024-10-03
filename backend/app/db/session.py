03492a94d3c8d818923bed967f78726694ad4716
vpakarinen <vpakarinen@proton.me>
2024-10-03 15:31:44 +0300

    Setup sqlite database session

---
+++ backend/app/db/session.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+engine = create_engine(
+    f"sqlite:///{settings.DATABASE_URL}",
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    """ Provides database session to the path operation functions """
+    db = SessionLocal()
+
+    try:
+        yield db
+    except Exception as e:
+        logger.error(f"Database session error: {e}")
+    finally:
+        db.close()
