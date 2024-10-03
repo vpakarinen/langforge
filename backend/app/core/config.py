cd87be24859be978e55ffa234cf7f777ce0aab98
vpakarinen <vpakarinen@proton.me>
2024-10-03 14:42:26 +0300

    Setup basic logging system

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings
 from pydantic import Field
+from pathlib import Path
 
 class Settings(BaseSettings):
     DATABASE_URL: str = Field("langforge.db", env="DATABASE_URL")
@@ -7,7 +8,7 @@ class Settings(BaseSettings):
     DEFAULT_MODEL: str = Field("Helsinki-NLP/opus-mt-en-es", env="DEFAULT_MODEL")
 
     class Config:
-        env_file = ".env"
+        env_file = Path(__file__).resolve().parent.parent.parent.parent /".env"
         env_file_encoding = "utf-8"
 
 settings = Settings()
---
+++ backend/app/core/logger.py
@@ -0,0 +1,9 @@
+import logging
+from app.core.config import settings
+
+def setup_logger():
+    """ Configure the logger for the application """
+    logging.basicConfig(
+        level=getattr(logging, settings.LOG_LEVEL.upper, logging.INFO),
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
