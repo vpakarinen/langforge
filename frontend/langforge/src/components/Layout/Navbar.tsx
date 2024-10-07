26f5e98f6d09a359befc88f71768a7ec5d1e23f6
vpakarinen <vpakarinen@proton.me>
2024-10-07 17:09:02 +0300

    Setup Navbar component and minor code fixes

---
+++ frontend/langforge/src/components/Layout/Navbar.tsx
@@ -0,0 +1,61 @@
+import Link from 'next/link';
+import { FaLanguage } from 'react-icons/fa';
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/react';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-600 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/">
+          <a className="flex items-center text-white text-xl">
+            <FaLanguage className="mr-2" />
+            LangForge
+          </a>
+        </Link>
+        <div className="flex space-x-4">
+          <Link href="/">
+            <a className="text-white hover:underline">Home</a>
+          </Link>
+          <Link href="/translate">
+            <a className="text-white hover:underline">Translate</a>
+          </Link>
+          <Menu as="div" className="relative inline-block text-left">
+            <MenuButton className="text-white hover:underline">More</MenuButton>
+            <MenuItems className="absolute right-0 mt-2 w-40 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="px-1 py-1">
+                <MenuItem>
+                  {({ active }) => (
+                    <Link href="/features">
+                      <a
+                        className={`${
+                          active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                        } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                      >
+                        Features
+                      </a>
+                    </Link>
+                  )}
+                </MenuItem>
+                <MenuItem>
+                  {({ active }) => (
+                    <Link href="/about">
+                      <a
+                        className={`${
+                          active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                        } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                      >
+                        About
+                      </a>
+                    </Link>
+                  )}
+                </MenuItem>
+              </div>
+            </MenuItems>
+          </Menu>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
