b663bf6025c96895f59f33f92b89b9c2535fa106
vpakarinen <vpakarinen@proton.me>
2024-10-07 17:33:29 +0300

    Setup Footer component

---
+++ frontend/langforge/src/components/Layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      <p>&copy;{new Date().getFullYear()} LangForge. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
