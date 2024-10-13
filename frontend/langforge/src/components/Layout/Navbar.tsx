08a87b451666e84df229577d7ef3ddfdcd7b9dd0
vpakarinen <vpakarinen@proton.me>
2024-10-13 19:20:22 +0300

    Small UI update

--- frontend/langforge/src/components/Layout/Navbar.tsx
+++ frontend/langforge/src/components/Layout/Navbar.tsx
@@ -3,16 +3,16 @@ import { FaLanguage } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <nav className="p-4">
+    <nav className="p-4 bg-gray-800">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="flex items-center text-3xl font-bold">
           <FaLanguage className="mr-2" />
         </Link>
         <div className="flex space-x-4">
-          <Link href="/" className="hover:underline">
+          <Link href="/" className="text-lg hover:underline">
             Home
           </Link>
-          <Link href="/translate" className="hover:underline">
+          <Link href="/translate" className="text-lg hover:underline">
             Translate
           </Link>
         </div>
--- frontend/langforge/src/pages/index.tsx
+++ frontend/langforge/src/pages/index.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link';
 
 const HomePage = () => {
   return (
-    <main className="flex items-center justify-center mt-10">
+    <main className="flex items-center justify-center mt-20">
       <div>
         <div className="text-center">
           <h1 className="text-4xl font-semibold mb-2">
             LangForge
           </h1>
-          <p className="text-lg mb-6">
+          <p className="text-lg mb-5">
             Translate text between multiple languages using local translation engine
           </p>
         <Link
